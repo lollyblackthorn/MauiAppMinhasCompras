e2504833389df1cbf550f5a77ea56d6c4bedd1cb
Lorrany <79610401+lollyblackthorn@users.noreply.github.com>
2025-03-31 04:19:52 -0300

    fazendo tratamento dos inputs do produto

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -5,6 +5,9 @@ namespace MauiAppMinhasCompras.Models
     public class Produto
     {
         string _descricao;
+        double _quantidade;
+        double _preco;
+
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
@@ -20,8 +23,32 @@ namespace MauiAppMinhasCompras.Models
                 _descricao = value;
             }
         }
-        public double Quantidade {  get; set; }
-        public double Preco {  get; set; }
+        public double Quantidade {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("A quantidade não pode ser 0");
+                }
+
+                _quantidade = value;
+            }
+
+        }
+        public double Preco {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("O preço não pode ser 0");
+                }
+
+                _preco = value;
+            }
+
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
